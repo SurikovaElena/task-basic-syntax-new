f840f5d096682cb5474ac309866fd2452aae7624
Surikova Elena <surikovaelena_1997@mail.ru>
2025-05-12 21:40:26 +0300

    Задание по римским цифрам

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -4,5 +4,18 @@ export function romanToInteger(str) {
     Напишите код, преобразующий число из римской записи в арабскую.
     На вход вы получите строку, ответом должно быть число.
      */
+    let length = str.length;
+    const romanNotation = ['I', 'V', 'X', 'L', 'C', 'D', 'M'],
+        arabicNotation = [1, 5, 10, 50, 100, 500, 1000];
+    for (let i = 0; i < length; i++) {
+        let idCurrentSymbol = romanNotation.indexOf(str[i]),
+            idNextSymbol = romanNotation.indexOf(str[i + 1]);
+        if (arabicNotation[idCurrentSymbol] < arabicNotation[idNextSymbol]) {
+            result += arabicNotation[idNextSymbol] - arabicNotation[idCurrentSymbol];
+            i++;
+        } else {
+            result += arabicNotation[idCurrentSymbol];
+        }
+    }
     return result;
 }
